53caebd2ebdc154d0eb282d78a960f4751744b6f
Joachim Grimstad <Joachim.Grimstad@IAS.uni-stuttgart.de>
2024-05-16 19:29:30 +0200

    cleanup

--- main.py
+++ main.py
@@ -15,16 +15,6 @@ min_children = 2
 
 if __name__ == '__main__':
 
-    # Generator 
-    # FT, id = FaultTreeGenerator.generate_fault_tree(start_id, max_depth, max_children, min_children)
-
-
-    # G = Converter.convert_to_graph(FT)
-
-    # FT = FaultTree(start_id, min_children, max_children, max_depth)
-    # for node in FT.nodes:
-    #     print(node.id, node.node_type)
-
     Gen = Generator(start_id, max_depth, max_children, min_children, num_models)
     G = Gen.generate_data()
 
