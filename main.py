201fdf01a3ba7d5902bd82ab619fc3bd20e8d76b
Yashaswini-Sreenivasa <167520761+Yashaswini-Sreenivasa@users.noreply.github.com>
2024-05-17 16:24:45 +0200

    Update main.py
    Added a feature to make the graph planar and to display the node attributes on the graph/console.

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import networkx as nx
 
 # Params
     # Data Params
-num_models = 4
+num_models = 1
     # Tree Params
     
 start_id = 0
-max_depth = 3
+max_depth = 5
 max_children = 3
 min_children = 2
 
@@ -17,6 +17,16 @@ if __name__ == '__main__':
 
     Gen = Generator(start_id, max_depth, max_children, min_children, num_models)
     G = Gen.generate_data()
+    
+    pos = nx.planar_layout(G) 
 
-    nx.draw(G)
+    '''
+    Displays node attributes on the graph
+    node_labels = {node: f"ID: {data['id']}\nP: {data.get('failure_probability', 'N/A')}"  
+                   for node, data in G.nodes(data=True)}
+    nx.draw(G, pos, with_labels=True, labels=node_labels, node_size=5000, node_color='lightblue', font_size=8)  
+    '''
+
+    #Displays node attributes on the console
+    nx.draw(G, pos, node_size=50, node_color='lightblue', font_size=8)
     plt.show()
